0e5b38fa244427c72fdb04b73c50e262f25edcc9
Lawrence Chan <chanlawrencet@gmail.com>
2019-10-12 10:56:20 -0400

    bson import

--- database/testDB.py
+++ database/testDB.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_pymongo import PyMongo
 import os
 import random
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 MONGO_URL = os.environ.get('MONGODB_URI')
-app.config["MONGO_URI"] = MONGO_URL
-# app.config["MONGO_URI"] = 'mongodb://localhost:27017/testDB'
+# app.config["MONGO_URI"] = MONGO_URL
+app.config["MONGO_URI"] = 'mongodb://localhost:27017/testDB'
 mongo = PyMongo(app)
 
 def makeRequest(targetID, email):
