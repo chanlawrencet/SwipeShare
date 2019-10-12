b7cc06661527a2f2929597ca04238094290043fe
Naoki Okada <naokiokada11@gmail.com>
2019-10-12 01:46:00 -0400

    Added backend endpoints

--- database/testDB.py
+++ database/testDB.py
@@ -5,23 +5,44 @@ import os
 app = Flask(__name__)
 MONGO_URL = os.environ.get('MONGODB_URI')
 app.config["MONGO_URI"] = MONGO_URL
+# app.config["MONGO_URI"] = 'mongodb://localhost:27017/testDB'
 mongo = PyMongo(app)
 
 
-def getTest():
-    contents = list(mongo.db.users.find())
+
+def addUser(new_user):
+    users = mongo.db.users
+    users.insert_one(new_user)
+
+    return
+
+def getEntries():
+    contents = list(mongo.db.entries.find())
     returnList = []
     for content in contents:
-        ele = {'name': content['name'], 'email': content['email']}
-        returnList.append(ele)
+        if content['receiver_email'] == '':
+            ele = {'giver_email': content['giver_email'], 
+                'receiver_email': content['receiver_email'],
+                'location': content['location'],
+                'time': content['time']}
+            returnList.append(ele)
     return returnList
 
-def makeUsers():
-    users = mongo.db.users
-    for i in range (0, 10):
-        post = {}
-        post['name'] = 'User ' + str(i)
-        post['email'] = 'User' + str(i) + '@tufts.edu'
-        users.insert_one(post)
+def addEntry(new_entry):
+    entries = mongo.db.entries
+    entries.insert_one(new_entry)
+
+    return
+
+# (def modifyEntry) this function adds the receiver email to entry
+def modifyEntry(entry, receiver):
+    entries = mongo.db.entries
+    entries.update_one(entry,{ '$set': receiver })
+
+    return
+
+def deleteEntries():
+    entries = mongo.db.entries
+    entries.remove()
 
     return
--- run.py
+++ run.py
@@ -1,39 +1,66 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse, abort
 from database import testDB
+import json
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('first', type=str)
+parser.add_argument('last', type=str)
+parser.add_argument('email', type=str)
+parser.add_argument('date', type=str)
+parser.add_argument('time', type=str)
+parser.add_argument('location', type=str)
 
 # look I'm a comment
 
-
-class HelloWorld(Resource):
-    def get(self):
-        listOfNums = []
-        for i in range(0, 10):
-            listOfNums.append(i)
-
-        testDB.getTest()
-        return {'users': testDB.getTest()}
-
-class MakeUsers(Resource):
-    def get(self):
-        testDB.makeUsers()
-        return {'success': True}
-
-class Demo(Resource):
-    def get(self):
-        return {
-            'hello': 'world',
-            'imalist': [1,2,3,4]
+class AddUser(Resource):
+    def post(self):
+        new_user = {
+            "email": request.json['email'],
         }
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Demo, '/demo')
-api.add_resource(MakeUsers, '/insert')
+        testDB.addUser(new_user)
+        return
 
+class GetEntries(Resource):
+    def get(self):
+        return {'entries': testDB.getEntries()}
+
+class AddEntry(Resource):
+    def post(self):
+        new_entry = {
+            "giver_email": request.json['giver_email'],
+            "receiver_email": request.json['receiver_email'],
+            "location": request.json['location'],
+            "time": request.json['time']
+        }
+
+        testDB.addEntry(new_entry)
+        return
+
+class FindEntry(Resource):
+    def put(self):
+        entry = {"giver_email":request.json['giver_email'],
+                 "location": request.json['location'],
+                 "time": request.json['time']
+            }
+        
+        receiver = { "receiver_email": request.json['receiver_email'] }
+        testDB.modifyEntry(entry, receiver)
+        return
+
+class DeleteEntries(Resource):
+    def post(self):
+        testDB.deleteEntries()
+        return
+
+api.add_resource(GetEntries, '/')
+api.add_resource(AddUser, '/adduser')
+api.add_resource(AddEntry, '/addentry')
+api.add_resource(FindEntry, '/findentry')
+api.add_resource(DeleteEntries, '/deleteentries')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
